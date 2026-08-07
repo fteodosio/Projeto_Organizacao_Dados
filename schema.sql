0eb6eaa70a939b3d947aa87c321a008b317210e5
fteodosio <fteodosio@gmail.com>
2026-08-07 11:16:26 +0100

    Update schema.sql
    Trigger histórico por Preços,
    Índices parciais analíticos por quantidade

--- schema.sql
+++ schema.sql
@@ -28,3 +28,38 @@ CREATE TABLE Vendas (
 -- Restrição para o email
 ALTER TABLE Utilizadores
 ADD CONSTRAINT email_valido CHECK (email LIKE '%@%.%'); 
+
+ALTER TABLE Produtos
+ADD CONSTRAINT preco_positivo CHECK (preco >= 0);
+
+ALTER TABLE Vendas
+ADD CONSTRAINT qtd_positiva CHECK (quantidade >= 0);
+
+-- 3. Aceleração
+CREATE INDEX idx_utilizador_nome ON Utilizadores(nome);
+CREATE INDEX idx_produto_titulo ON produtos(titulo);
+CREATE INDEX idx_produtos_mais_vendidos
+ON Vendas (quantidade)
+WHERE quantidade >= 10; 
+-- 4. Segurança
+CREATE ROLE estagiario_leitor;
+GRANT SELECT ON Utilizadores TO estagiario_leitor;
+-- 5. Auditoria
+CREATE TABLE Auditoria_Precos (
+ produto_id INT,
+ preco_antigo DECIMAL,
+ preco_novo DECIMAL,
+ data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- O Porteiro (Trigger)
+CREATE OR REPLACE FUNCTION registar_alteracao()
+RETURNS TRIGGER AS $$
+BEGIN
+ INSERT INTO Auditoria_Precos (produto_id, preco_antigo, preco_novo)
+ VALUES (OLD.id, OLD.preco, NEW.preco);
+ RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER porteiro_precos
+AFTER UPDATE ON Produtos
+FOR EACH ROW EXECUTE FUNCTION registar_alteracao();
